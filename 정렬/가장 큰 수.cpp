ffcb2ba81e220f6992f8815484fdcc23e8725c7f
KWANGWOO LEE <dlrhkddn0325@gmail.com>
2021-06-24 16:43:21 +0900

    Update 가장 큰 수.cpp
    add comment, modify include

--- 정렬/가장 큰 수.cpp
+++ 정렬/가장 큰 수.cpp
@@ -1,9 +1,6 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <algorithm>
-#include <vector>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,18 +8,23 @@ bool condition(int& _a, int& _b){
     string sa = to_string(_a);
     string sb = to_string(_b);
 
+    // 두 수를 ab, ba 로 붙여서대소 비교
     string strcat1 = sa + sb;
     string strcat2 = sb + sa;
 
+    // 오름차순 상태면 a >_ b 임.
+    // return 이 false 면 변경x, true면 변경o
     return stoi(strcat1) > stoi(strcat2);
 }
 
 string solution(vector<int> numbers) {
+
     vector<int> my;
     for(auto e:numbers){
         my.push_back(e);
     }
 
+    // condition 조건에 맞게 정렬
     sort(my.begin(), my.end(), condition);
 
     string answer;
@@ -34,6 +36,7 @@ string solution(vector<int> numbers) {
         }
     }
     
+    // 000 과 같은 경우 0으로 출력
     if(answer[0] == '0'){
         answer = "0";
     }
