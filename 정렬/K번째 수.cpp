04f74bf8cf7978cfed7686594634e19ed1d62670
KWANGWOO LEE <dlrhkddn0325@gmail.com>
2021-08-11 22:22:57 +0900

    Update K번째 수.cpp

--- 정렬/K번째 수.cpp
+++ 정렬/K번째 수.cpp
@@ -7,31 +7,16 @@ using namespace std;
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     
-    int start = 0;
-    int end = 0;
-    int target = 0;
-    int wait_cnt = 1;
-    vector<int> temp;
+    for(auto e:commands){
+        // iterator 이용해서 자르기
+        // 타입 변수명(.begin(), .end())
+        vector<int> temp(array.begin()+e[0]-1, array.begin()+e[1]);
         
-    for(int i=0; i<commands.size(); i++){
-        start = commands[i][0];
-        end = commands[i][1];
-        target = commands[i][2];
-        
-        // 자르기
-        for(auto& e:array){
-            if((wait_cnt >= start) & (wait_cnt <= end)){
-                temp.push_back(e);
-            }
-            wait_cnt += 1;
-        }
         // 정렬
         sort(temp.begin(), temp.end());
-        // 값 삽입
-        answer.push_back(temp[target-1]);
-        // 초기화
-        temp.clear();
-        wait_cnt = 1;
+        
+        // 삽입
+        answer.push_back(temp[e[2]-1]);
     }
     
     return answer;
