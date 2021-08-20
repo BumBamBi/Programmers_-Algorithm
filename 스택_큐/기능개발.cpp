f937b2e0cd3e559856f387bf5fb5cb7731fe9a80
BumBamBi <dlrhkddn0325@gmail.com>
2021-08-20 13:03:31 +0900

    update 기능개발

--- 스택_큐/기능개발.cpp
+++ 스택_큐/기능개발.cpp
@@ -9,22 +9,32 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     
     stack<int> mystack;
     
+    // 필요한 날짜만큼 stack에 역으로 저장.(가장 마지막 기능이 먼저 push)
     for(int i=progresses.size()-1; i>=0; i--){
+        // (남은 작업진도 / 속도) 올림한 값
+        // (100-93)/3 = 2.xx 이라 3일이 걸리지만, int형은 내림 하므로 2일이 된다.
+        // double형으로 하여 2.xx 로 바꾸고 올림하고 int형으로 재 변경.
         mystack.push((int)ceil((100.0 - progresses[i]) / speeds[i]));
     }
     
-    int temp = mystack.top();
-    int cnt = 0;
+    // 정답을 넣을 벡터
     vector<int> answer;
+    // 스택의 최초 top값을 저장할 변수
+    int before_top = mystack.top();
+    // 한번에 pop한 수를 카운트하는 변수
+    int cnt = 0;
     
+    // 스택에 있는값을 전부 pop
     while(!mystack.empty()){
-        if(temp >= mystack.top()){
+        if(before_top >= mystack.top()){
+            // 차례로 pop하되, top에 있는 값보다 작은 값들은 연속으로 pop
             mystack.pop();
             cnt += 1;
         }else{
+            // 한번에 pop한 수를 answer에 push하고, before_top변수 초기화
             answer.push_back(cnt);
             cnt = 0;
-            temp = mystack.top();
+            before_top = mystack.top();
         }
     }
     answer.push_back(cnt);
