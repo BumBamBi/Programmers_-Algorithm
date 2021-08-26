80840372cc0d44faec5bbb00f13a00e6e964a98f
BumBamBi <dlrhkddn0325@gmail.com>
2021-08-26 22:30:23 +0900

    add comments for 주식가격

--- 스택_큐/주식가격.cpp
+++ 스택_큐/주식가격.cpp
@@ -9,16 +9,22 @@ vector<int> solution(vector<int> prices) {
     int std = 0;
     int cnt = 0;
     
+    // prices 만큼 반복
     for(int i=0; i<prices.size(); i++){
         std = prices[i];
+        // 선택한 요소 다음 요소부터 반복
         for(int j=i+1; j<prices.size(); j++){
-            if(std <= prices[j]){
-                cnt += 1;
-            }else{
+            // 가격이 떨어진다면
+            if(std > prices[j]){
                 cnt += 1;
                 break;
             }
+            // 가격이 떨이지지 않는다면
+            else{
+                cnt += 1;
+            }
         }
+        // 한 요소에 대한 결과 삽입 후 초기화
         answer.push_back(cnt);
         cnt = 0;
     }
