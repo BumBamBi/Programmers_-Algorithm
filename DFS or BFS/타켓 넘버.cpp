1c92dbc2c18772e70014d9a8e3a089e3cb9ebb30
BumBamBi <dlrhkddn0325@gmail.com>
2021-10-06 19:46:49 +0900

    solve 타겟 넘버

--- DFS or BFS/타켓 넘버.cpp
+++ DFS or BFS/타켓 넘버.cpp
@@ -2,17 +2,24 @@
 #include <vector>
 
 using namespace std;
+
+// 전역변수로 answer을 미리 선언
 int answer = 0;
 
 void dfs(vector<int> numbers, int target, int sum, int count){
-    if (count == numbers.size()){
-        if (sum == target){
-            answer++;
+    // numbers의 모든 요소에 접근했을 때
+    if(count == numbers.size()){
+        // 그 값이 target과 같다면
+        if(sum == target){
+            answer +=1 ;
         }
         return;
-    }
+    }else{
+        // 현재 numbers의 요소를 + 할 경우
         dfs(numbers, target, sum + numbers[count], count + 1);
+        // 현재 numbers의 요소를 - 할 경우
         dfs(numbers, target, sum - numbers[count], count + 1);
+    }
 }
 
 int solution(vector<int> numbers, int target) {
