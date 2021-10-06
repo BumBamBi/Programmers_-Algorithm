420db5012cec71c169534205bcb4734fc97fba51
BumBamBi <dlrhkddn0325@gmail.com>
2021-10-07 00:25:31 +0900

    solve 네트워크

--- DFS or BFS/네트워크.cpp
+++ DFS or BFS/네트워크.cpp
@@ -4,33 +4,34 @@
 
 using namespace std;
 
-void dfs(vector<vector<int>> _v, bool visited[], int& answer){
-    // 모든 노드를 검사
-    for(int i=0; i<_v.size(); i++){
-        // 이미 확인했던 노드가 아니라면
-        if(visited[i] == false){
-            // 확인 한 것으로 바꾸고
-            visited[i] = true;
-            
-            // 독립된 노드라면 + 1
-            if(count(_v[i].begin(), _v[i].end(), 1) == 1){
-                answer += 1;
-            }
-            // 독립된 노드가 아니라면 다시 확인
-            else{
-                dfs(_v, visited, answer);
+int dfs(int i, vector<vector<int>> computers, bool visited[]){
+    if(visited[i] == true){
+        return 0;
+    }else{
+        visited[i] = true;
+        
+        // 현재 노드와 연결 된 노드가 더 있는지 찾아보기
+        for(int j=0; j<computers[i].size(); j++){
+            // 연결 된 노드가 있다면
+            if(computers[i][j] == 1){
+                dfs(j, computers, visited);
             }
         }
+        return 1;
     }
-    return;
 }
 
 int solution(int n, vector<vector<int>> computers) {
-    
+    int answer = 0;
     bool* visited = new bool[n]{false};
-    int answer = 1;
     
-    dfs(computers, visited, answer);
-
+    // 모든 노드에 접근
+    for(int i=0; i<n; i++){
+        // 방문하지 않았던 노드라면 dfs를 거쳐 추가
+        if(visited[i] == false){
+            answer += dfs(i, computers, visited);
+        }
+    }
+    
     return answer;
 }
