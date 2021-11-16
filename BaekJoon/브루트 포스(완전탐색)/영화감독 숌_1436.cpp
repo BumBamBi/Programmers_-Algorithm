cc30749f5824ee9497cebddb8dcfd4eb87b87ddd
BumBamBi <dlrhkddn0325@gmail.com>
2021-11-17 02:23:04 +0900

    [ADD] 별찍기

---
+++ BaekJoon/브루트 포스(완전탐색)/영화감독 숌_1436.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    // 입력 --------------------------
+    
+    // -------------------------------
+
+    return 0;
+}
