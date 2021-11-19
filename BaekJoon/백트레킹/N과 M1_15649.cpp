091ef7c72ff714bdbf9371b08c3a8ede5af78a23
BumBamBi <dlrhkddn0325@gmail.com>
2021-11-20 02:32:56 +0900

    [MOD] N과M1 코드 수정

--- BaekJoon/백트레킹/N과 M1_15649.cpp
+++ BaekJoon/백트레킹/N과 M1_15649.cpp
@@ -12,32 +12,34 @@ int main(){
     cin >> N >> len;
     // -------------------------------
 
-    vector<string> v;
+    vector<int> v;
     for(int i=1; i<=N; i++){
-        v.push_back(to_string(i));
+        v.push_back(i);
     }
 
-
     string pre_str = "";
-    for(int i=0; i<len; i++){
-        pre_str += v[i];
+    for(int i=0; i<len;i++){
+        pre_str += to_string(v[i]);
         cout << v[i] << ' ';
     }
     cout << '\n';
 
     do{
         string temp_str = "";
-        for(int i=0; i<len; i++){
-            temp_str += v[i];
+        for(int i=0; i<len;i++){
+            temp_str += to_string(v[i]);
         }
-        if(pre_str.compare(temp_str) != 0){
+
+        if(temp_str.compare(pre_str) != 0){
+            pre_str = temp_str;
             for(int i=0; i<len; i++){
                 cout << v[i] << ' ';
             }
             cout << '\n';
-            pre_str = temp_str;
         }
+
     }while(next_permutation(v.begin(), v.end()));
+
 
     return 0;
 }
