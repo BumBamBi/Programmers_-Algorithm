6e69633bac925dab1d7d62a694b42487ed7c456c
BumBamBi <dlrhkddn0325@gmail.com>
2021-11-23 18:48:50 +0900

    [ADD] N과 M3

---
+++ BaekJoon/백트레킹/N과 M3_15651.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void func(int N, int M, vector<int> visited, int len) {
+	if (len == M) {
+		for(auto e:visited){
+            cout << e << ' ';
+        }
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+        visited.push_back(i);
+		func(N, M, visited, len + 1);
+        visited.pop_back();
+	}
+}
+
+int main() {
+	// 입력 --------------------------
+    int N, M;
+    cin >> N >> M;
+    // -------------------------------
+
+    vector<int> visited;
+
+    func(N, M, visited, 0);
+
+    return 0;
+}
