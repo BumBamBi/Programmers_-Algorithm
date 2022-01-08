59c9cd626b727911f7a2aefdafa9657f62df3396
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-08 21:04:26 +0900

    solve 그룹 단어 체커

---
+++ BaekJoon/문자열/그룹 단어 체커_1316.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+bool func(char * str){
+    char alpha[26] = {};
+
+    char temp = str[0];
+    for(int i=1; i<strlen(str); i++){
+        if(temp != str[i]){
+            if(alpha[temp-97] != 0){
+                return false;
+            }else{
+                alpha[temp-97] = 1;
+                temp = str[i];
+            }
+        }
+    }
+    if(alpha[temp-97] != 0){
+        return false;
+    }
+    return true;
+}
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+
+    
+    int result = 0;
+    for(int i=0; i<N; i++){
+        char arr[101];
+        scanf("%s", arr);
+
+        if(func(arr)){
+            result+=1;
+        }
+    }
+    printf("%d", result);
+}
