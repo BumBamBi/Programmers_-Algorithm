b3cce4194b738ad85be4eff795ba842101841f88
BumBamBi <dlrhkddn0325@gmail.com>
2022-10-10 00:20:13 +0900

    [ADD] solve 야근 지수

--- Programmers/야근 지수.cpp
+++ Programmers/야근 지수.cpp
@@ -1,22 +1,35 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <queue>
 #include <math.h>
+#include <iostream>
 
 using namespace std;
 
 long long solution(int n, vector<int> works) {
     
+    priority_queue<int, vector<int>> pq;
+    
+    for(auto e:works){
+        pq.push(e);
+    }
+    
     for(int i=0; i<n; i++){
-        int maxNumIndex = max_element(works.begin(), works.end()) - works.begin();
-        if(works[maxNumIndex] > 0){
-            works[maxNumIndex]--;
+        int maxNum = pq.top();
+        if(maxNum == 0){
+            break;
+        }else{
+            pq.pop();
+            pq.push(maxNum-1);
         }
     }
     
     long long answer = 0;
-    for(auto e:works){
-        answer += pow(e,2);
+    while(!pq.empty()){
+        int top = pq.top();
+        pq.pop();
+        answer += (long long)pow(top,2);
     }
     return answer;
 }
