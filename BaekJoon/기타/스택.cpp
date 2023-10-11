a225a653d047c276d2a9df80189cdbc23fdfae0c
BumBamBi <dlrhkddn0325@gmail.com>
2023-10-11 23:38:02 +0900

    [MOD] update Readme

---
+++ BaekJoon/기타/스택.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int N;
+
+int main(){
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++){
+        string s;
+        cin >> s;
+    }
+
+        return 0;
+}
