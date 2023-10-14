b13109b07cc41da1bff9eff73429cb4bba7ee8b1
BumBamBi <dlrhkddn0325@gmail.com>
2023-10-14 15:28:47 +0900

    [ADD] K번쨰 수

--- Programmers/정렬/K번째 수2.cpp
+++ Programmers/정렬/K번째 수2.cpp
@@ -1,23 +1,19 @@
-#include <string>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    
     vector<int> answer;
     
     for(auto e:commands){
-        // iterator 이용해서 자르기
-        // 타입 변수명(.begin(), .end())
-        vector<int> temp(array.begin()+e[0]-1, array.begin()+e[1]);
+        vector<int> v(array.begin()+e[0]-1, array.begin()+e[1]);
         
-        // 정렬
-        sort(temp.begin(), temp.end());
+        sort(v.begin(), v.end());
         
-        // 삽입
-        answer.push_back(temp[e[2]-1]);
+        answer.push_back(v[e[2]-1]);
     }
+    
     
     return answer;
 }
